9571a24d4c3158c92cc2e7a803f5f2ce5aff2662
rendi purnomo <rendipur234@gmail.com>
2024-02-12 04:16:47 +0700

    initialize author

--- server.js
+++ server.js
@@ -22,6 +22,15 @@ app.use(express.urlencoded({ extended: true }));
 connectToGoogleSheet();
 app.use(userRoute);
 
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to the Google Sheet API",
+    status: 200,
+    author: "Rendi Purnomo",
+    github: "https://github.com/rendipurnomo"
+  });
+})
+
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port " + process.env.PORT);
 });
